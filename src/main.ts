0596f27221a81d928a529257a2e055f8b12162e2
angieene <angela.nerukh@gmail.com>
2023-02-14 16:14:34 +0200

    cors header

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
       preflightContinue: false,
       optionsSuccessStatus: 204,
-      allowedHeaders: ['content-type'],
+      allowedHeaders: ['content-type', 'application/json'],
       credentials: true,
     },
   });
