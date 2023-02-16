36815609fbd57485274826fa15c6ec582bad94a0
angieene <angela.nerukh@gmail.com>
2023-02-16 17:03:13 +0200

    try 4

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,21 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
 
   /* app.enableCors({
     origin: ['http://localhost:3000', 'https://api-filmgen-pearl.vercel.app'],
     methods: ['GET', 'POST'],
     credentials: true,
   });*/
+
+  app.enableCors({
+    credentials: true,
+    origin: ['http://localhost:3000', 'https://api-filmgen-pearl.vercel.app'],
+    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+  });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.use('/favicon-*', (_, res) => res.send());
