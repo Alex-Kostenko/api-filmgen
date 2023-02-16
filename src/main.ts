3cfddc501b75eb0eae12a82e409d52d4bdc1822b
angieene <angela.nerukh@gmail.com>
2023-02-16 11:45:59 +0200

    cors true

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,13 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
-  app.enableCors({
+  /* app.enableCors({
     origin: ['http://localhost:3000', 'https://api-filmgen-pearl.vercel.app'],
     methods: ['GET', 'POST'],
     credentials: true,
-  });
+  });*/
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.use('/favicon-*', (_, res) => res.send());
