e788634324542c16c4bfd690431c72d67dfc568c
angieene <angela.nerukh@gmail.com>
2023-02-16 10:18:57 +0200

    cors try1

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [/localhost:\d+$/, /\.vercel\.app$/, /\.herokuapp\.com$/],
+    allowedHeaders: ['content-type', 'accept', 'origin', 'application/json'],
+    credentials: true,
+  });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.use('/favicon-*', (_, res) => res.send());
