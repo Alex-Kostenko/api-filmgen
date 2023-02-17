37f00e27c175df95d8cc50a7217e4e5e91806e36
angieene <angela.nerukh@gmail.com>
2023-02-17 15:13:52 +0200

    lint

--- src/main.ts
+++ src/main.ts
@@ -17,27 +17,6 @@ async function bootstrap() {
       'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
   });
 
-  // let whitelist = [
-  //   'http://localhost:3000',
-  //   'https://api-filmgen-pearl.vercel.app',
-  // ];
-
-  // app.enableCors({
-  //   credentials: true,
-  //   origin: function (origin, callback) {
-  //     if (whitelist.indexOf(origin) !== -1) {
-  //       console.log('allowed cors for:', origin);
-  //       callback(null, true);
-  //     } else {
-  //       console.log('blocked cors for:', origin);
-  //       callback(new Error('Not allowed by CORS'));
-  //     }
-  //   },
-  //   methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
-  //   allowedHeaders:
-  //     'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-  // });
-
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.use('/favicon-*', (_, res) => res.send());
 
