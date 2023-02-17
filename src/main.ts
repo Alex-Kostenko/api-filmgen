720b87bf28e0dfa5f254fd7673eead44dac6577f
angieene <angela.nerukh@gmail.com>
2023-02-17 10:58:37 +0200

    no cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
 
   /* app.enableCors({
     origin: ['http://localhost:3000', 'https://api-filmgen-pearl.vercel.app'],
