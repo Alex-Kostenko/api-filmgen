2a8668f2cf87a4727726daa2331161dde1014952
angieene <angela.nerukh@gmail.com>
2023-02-27 10:49:37 +0200

    add two column to genres

--- src/genres/entities/genre.entity.ts
+++ src/genres/entities/genre.entity.ts
@@ -10,4 +10,12 @@ export class GenreEntity {
   @ApiProperty({ type: String })
   @Column({ type: 'varchar', length: '400', nullable: false })
   name: string;
+
+  @ApiProperty({ type: String })
+  @Column({ type: 'varchar', length: '400', nullable: true })
+  name_eng: string;
+
+  @ApiProperty({ type: String })
+  @Column({ type: 'varchar', length: '400', nullable: true })
+  name_ukr: string;
 }
