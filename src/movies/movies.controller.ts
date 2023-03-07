6243091fd35a2dbf765f3b1410743cc5454eacb3
angieene <angela.nerukh@gmail.com>
2023-03-07 16:05:37 +0200

    return first result

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -11,7 +11,7 @@ import { PaginationBodyDTO } from './dto/pagination-body.dto';
 import { PaginationResDTO } from './dto/pagination.result.dto';
 import { MovieEntity } from './entities/movie.entity';
 import MoviesService from './movies.service';
-import { IMoviesUrls, IMovieTrailerResult } from './types/main';
+import { IMoviesUrls, IMovieTrailer } from './types/main';
 
 @ApiTags('Movies')
 @Controller('movies')
@@ -63,7 +63,7 @@ export class MoviesController {
   @Get('get-trailer/:movieId')
   async findTrailer(
     @Param() getMovieByIdDto: GetMovieByIdDto,
-  ): Promise<IMovieTrailerResult> {
+  ): Promise<IMovieTrailer> {
     return this.moviesService.findTrailer(getMovieByIdDto.movieId);
   }
 
